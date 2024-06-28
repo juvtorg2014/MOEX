608732593e73db70eecd8c82865e3993abb42881
juvtorg2014 <juvtorg2014@YANDEX.RU>
2024-06-28 14:32:50 +0300

    Загрузка нескольких фьючерсов - 20240628

--- oi_futures.py
+++ oi_futures.py
@@ -1,20 +1,22 @@
-import re
-import time
 import os
-from datetime import date
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 import pandas as pd
 
 
 COLUMN = [1, 2, 3, 4, 5]
 ROW = [1, 2, 3]
 URL = 'https://www.moex.com/ru/contract.aspx?code='
+futures_1 = ['SBRF-9.24', 'GAZR-9.24', 'LKOH-9.24', 'SNGP-9.24', 'TCSI-9.24',
+            'CNY-9.24', 'Si-9.24', 'CNYRUBF', 'GOLD-9.24', 'MIX-9.24', 'USDRUBF'
+           'SILV-9.24', 'ED-9.24', 'SPYF-9.24']
+futures = ['SBRF-9.24', 'GAZR-9.24', 'LKOH-9.24']
 DIR_NAME = os.getcwd()
+
+
 def split_dict(dic, nom):
     """Разделение словаря на несколько словарей"""
     keys = list(dic.keys())
@@ -22,9 +24,11 @@ def split_dict(dic, nom):
         yield {k: dic[k] for k in keys[i: i + nom]}
 
 
+
 def change_keys(chunks) -> list:
     """Замена цифрового ключа на символьный"""
     replacements = ['Code', 'Price', 'D_Pr', 'Vol_rub', 'Vol_con', 'OI', 'D_OI', 'Ras', 'Day']
+    len_list = len(chunks)
     new_dict_1 = {}
     new_dict_2 = {}
     new_dict_3 = {}
@@ -32,50 +36,86 @@ def change_keys(chunks) -> list:
     new_chunks = []
 
     for nn, item in enumerate(chunks, start=1):
-        if nn == 1:
+        if nn == 1 and nn <= len_list:
             new_dict_1 = dict(zip(replacements, list(item.values())))
             new_chunks.append(new_dict_1)
-        elif nn == 2:
+        elif nn == 2 and nn <= len_list:
             new_dict_2 = dict(zip(replacements, list(item.values())))
             new_chunks.append(new_dict_2)
-        elif nn == 3:
+        elif nn == 3 and nn <= len_list:
             new_dict_3 = dict(zip(replacements, list(item.values())))
             new_chunks.append(new_dict_3)
-        elif nn == 4:
+        elif nn == 4 and nn <= len_list:
             new_dict_4 = dict(zip(replacements, list(item.values())))
             new_chunks.append(new_dict_4)
     return new_chunks
 
 
-def connection(url):
+def get_oi(list_tag) -> list:
+    """Получение данных по открытому интересу"""
+    list_oi = []
+    for num, tag in enumerate(list_tag):
+        if tag.text == 'Открытые позиции': # list_tag[78]
+            oi_long_p = list_tag[num + 1].text.replace(',', '')
+            oi_short_p = list_tag[num + 2].text.replace(',', '')
+            oi_long_c = list_tag[num + 3].text.replace(',', '')
+            oi_short_c = list_tag[num + 4].text.replace(',', '')
+            oi_summ = list_tag[num + 5].text.replace(',', '')
+        elif tag.text == 'Изменение': # list_tag[78]
+            ch_long_p = list_tag[num + 1].text.replace(',', '')
+            ch_short_p = list_tag[num + 2].text.replace(',', '')
+            ch_long_c = list_tag[num + 3].text.replace(',', '')
+            ch_short_c = list_tag[num + 4].text.replace(',', '')
+            ch_summ = list_tag[num + 5].text.replace(',', '')
+        elif tag.text == 'Количество лиц':
+            qty_long_p = list_tag[num + 1].text.replace(',', '')
+            qty_short_p = list_tag[num + 2].text.replace(',', '')
+            qty_long_c = list_tag[num + 3].text.replace(',', '')
+            qty_short_c = list_tag[num + 4].text.replace(',', '')
+            qty_summ = list_tag[num + 5].text.replace(',', '')
+
+    OI = {'1_Long': int(oi_long_p), '1_Short': int(oi_short_p), '2_Long': int(oi_long_c),'2_Short': int(oi_short_c),'Summa': int(oi_summ)}
+    Change = {'1_Long': int(ch_long_p),'1_Short': int(ch_short_p),'2_Long': int(ch_long_c),'2_Short': int(ch_short_c),'Summa': int(ch_summ)}
+    Quantity = {'1_Long': int(qty_long_p),'1_Short': int(qty_short_p),'2_Long': int(qty_long_c),'2_Short': int(qty_short_c),'Summa': int(qty_summ)}
+    list_oi.append(OI)
+    list_oi.append(Change)
+    list_oi.append(Quantity)
+    return list_oi
+
+
+def connect(instrument):
     num = int('1')
     list_tag = []
-    url_tiker = URL + url
-    driver = webdriver.Firefox()
+    list_oi = []
+    url_tiker = URL + instrument
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Firefox(options=options)
     driver.get(url_tiker)
     if driver is None:
         print(f" Нет ответа от сервера {url_tiker}")
         exit()
     driver.maximize_window()
     WebDriverWait(driver, 5)
+    list_cookies = driver.get_cookies()
+    for cookies in list_cookies:
+        driver.add_cookie(cookies)
     button = driver.find_element(By.LINK_TEXT, "Согласен")
     WebDriverWait(driver, 5)
     if button is not None:
         button.send_keys(Keys.ENTER)
-    # Создание таблицы pandas
-    mydata = pd.read_csv('sample.txt')
-    OI = []
-    Change = []
-    Quantity = []
-    mydata = pd.read_csv('sample.txt')
-    # driver.refresh()
+
     WebDriverWait(driver, 10)
-    data = driver.find_element(By.TAG_NAME, "td").text.split()[0]
-    data_file = data[-4:] + '_' + data[3:5] + '_' + data[0:2]
-    #date_date = driver.find_element(By.ID, "optDate")
+    data = driver.find_element(By.TAG_NAME, "td")
+    if data.text != '':
+        data_need = data.text.split()[0]
+        data_file = data_need[-4:] + '_' + data_need[3:5] + '_' + data_need[0:2]
+    else:
+        data_need = driver.find_element(By.ID, "digest_refresh_time").text.split()[0]
+        data_file = data_need[-4:] + '_' + data_need[3:5] + '_' + data_need[0:2]
     contracts = driver.find_element(By.CLASS_NAME, 'ui-row')
-    #list_tbody = contracts.find_element(By.TAG_NAME, 'tbody').text.split('\n')
     list_web = contracts.find_elements(By.CLASS_NAME, 'ui-table-cell')
+    print(url_tiker)
 
     new_dic = {}
     chunk = []
@@ -91,54 +131,25 @@ def connection(url):
         chunk.append(item)
 
     chunks = change_keys(chunk)
-    df = pd.DataFrame(chunks)
-    file_name = DIR_NAME + '\\' + instrument + '_' + data_file + '_sum.csv'
-    df.to_csv(file_name, index=False)
-    print(df)
+    file_name_1 = DIR_NAME + '\\' + instrument + '_' + data_file + '_sum.csv'
+    save_file(chunks, file_name_1)
 
     list_tag = driver.find_elements(By.TAG_NAME, "td")
-    for num, tag in enumerate(list_tag):
-        if tag.text == 'Открытые позиции': # list_tag[78]
-            oi_long_p = list_tag[num + 1].text
-            oi_short_p = list_tag[num + 2].text
-            oi_long_c = list_tag[num + 3].text
-            oi_short_c = list_tag[num + 4].text
-            oi_summ = list_tag[num + 5].text
-        elif tag.text == 'Изменение': # list_tag[78]
-            ch_long_p = list_tag[num + 1].text
-            ch_short_p = list_tag[num + 2].text
-            ch_long_c = list_tag[num + 3].text
-            ch_short_c = list_tag[num + 4].text
-            ch_summ = list_tag[num + 5].text
-        elif tag.text == 'Количество лиц':
-            qty_long_p = list_tag[num + 1].text
-            qty_short_p = list_tag[num + 2].text
-            qty_long_c = list_tag[num + 3].text
-            qty_short_c = list_tag[num + 4].text
-            qty_summ = list_tag[num + 5].text
-    OI = [int(oi_long_p), oi_short_p, oi_long_c, oi_short_c, oi_summ]
-    Change = [ch_long_p, ch_short_p, ch_long_c, ch_short_c, ch_summ]
-    Quantity = [qty_long_p, qty_short_p, qty_long_c, qty_short_c, qty_summ]
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    # table = soup.find('table', class_="contract-open-positions table1")
-    # if table is not None:
-    #     for row in ROW:
-    #         for column in COLUMN:
-    #             if row == 1:
-    #                 oi = int(table)
-    #             elif row == 2:
-    #                 change = int(table.find_all('tr')[1:][ROW].contents[COLUMN].text)
-    #                 Change.append(change)
-    #             elif row == 3:
-    #                 quantity = int(table.find_all('tr')[1:][ROW].contents[COLUMN].text)
-    #                 Quantity.append(quantity)
+    list_oi = get_oi(list_tag)
+    file_name_2 = DIR_NAME + '\\' + instrument + '_' + data_file + '_oi.csv'
+    save_file(list_oi, file_name_2)
 
-    #mydata.to_csv(data_file +'.csv', index=False)
     driver.close()
+    driver.quit()
+
+def save_file(list_data, name_file):
+    df = pd.DataFrame(list_data)
+    df.to_csv(name_file, index=False)
+    print(df)
+
 
 if __name__ == '__main__':
     #instrument = input("Введите инструмент для сбора данных:\n")
-    instrument = 'SBRF-9.24'
-    connection(instrument)
+    for futur in futures:
+        connect(futur)
 
